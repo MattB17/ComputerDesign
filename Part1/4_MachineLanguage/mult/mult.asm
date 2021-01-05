50dcaafc9497273137dbce8498fe5160c76ba1ab
MattB17 <matt.buckley1755@gmail.com>
2021-01-05 12:55:18 -0500

    Pseudo code for Mult

--- Part1/4_MachineLanguage/mult/mult.asm
+++ Part1/4_MachineLanguage/mult/mult.asm
@@ -1,9 +1,21 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Implementation by Matt Buckley
+
+// Pseudo-Code
+//    a = R0
+//    b = R1
+//    idx = 0
+//    prod = 0
+//  LOOP:
+//    if idx >= b goto STOP
+//    prod = prod + a
+//    idx = idx + 1
+//    goto LOOP
+//  STOP:
+//    R2 = prod
