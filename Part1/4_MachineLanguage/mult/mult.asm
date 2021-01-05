33d70c4b743e950dafcf2a31322eda073f40fa57
MattB17 <matt.buckley1755@gmail.com>
2021-01-05 15:31:07 -0500

    Finishing mult

--- Part1/4_MachineLanguage/mult/mult.asm
+++ Part1/4_MachineLanguage/mult/mult.asm
@@ -19,3 +19,61 @@
 //    goto LOOP
 //  STOP:
 //    R2 = prod
+
+  // Load value of R0 (RAM[0]) into register D
+  // then save that value into memory location referenced by variable a
+  @R0
+  D=M
+  @a
+  M=D
+  // Load value of R1 (RAM[1]) into register D
+  // then save that value into memory location referenced by variable b
+  @R1
+  D=M
+  @b
+  M=D
+  // Set R2 to 0
+  @R2
+  M=0
+  // Set memory location referenced by idx to value 0
+  @idx
+  M=0
+  // Set memory location referenced by prod to value 0
+  @prod
+  M=0
+
+(LOOP)
+  // get current value of idx and subtract b from it
+  // if the value is at least 0, end the loop
+  @idx
+  D=M
+  @b
+  D=D-M
+  @STOP
+  D;JGE
+  // get the value of prod and add a to it
+  // then put it back in the memory location of prod
+  @prod
+  D=M
+  @a
+  D=D+M
+  @prod
+  M=D
+  // increment idx and return to top of loop
+  @idx
+  M=M+1
+  @LOOP
+  0;JMP
+
+(STOP)
+  // read value of prod and save it to memory location
+  // referenced by R2 (RAM[2])
+  @prod
+  D=M
+  @R2
+  M=D
+
+(END)
+  // infinite loop to end program
+  @END
+  0;JMP
