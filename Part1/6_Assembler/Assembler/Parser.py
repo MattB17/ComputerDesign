e808d40c8980b69e831b8177f05992be43a2f47b
MattB17 <matt.buckley1755@gmail.com>
2021-01-12 11:14:09 -0500

    Adding open and close methods to Parser

--- Part1/6_Assembler/Assembler/Parser.py
+++ Part1/6_Assembler/Assembler/Parser.py
@@ -3,5 +3,55 @@
 """
 
 class Parser:
-    def __init__(self, assembly_file):
-        self._file = assembly_file
+    """A class used to read and parse an assembly file.
+
+    Parameters
+    ----------
+    file_path: str
+        A string representing the path to an assembly file
+
+    Attributes
+    ----------
+    _file_path: str
+        A string representing the path to the assembly file being parsed.
+    _file: object
+        Represents the file being parsed
+
+    """
+    def __init__(self, file_path):
+        self._file_path = file_path
+        self._file = None
+
+    def get_file_path(self):
+        """The path to the file being parsed.
+
+        Returns
+        -------
+        str
+            A string representing the path of the file being parsed.
+
+        """
+        return self._file_path
+
+    def open_file(self):
+        """Opens the assembly file to be parsed.
+
+        Returns
+        -------
+        None
+
+        """
+        with open(self._file_path, 'r') as assembly_file:
+            self._file = assembly_file
+
+    def close_file(self):
+        """Closes the assembly file.
+
+        Returns
+        -------
+        None
+
+        """
+        if self._file:
+            self._file.close()
+            self._file = None
--- Part1/6_Assembler/Assembler/tests/test_parser.py
+++ Part1/6_Assembler/Assembler/tests/test_parser.py
@@ -1,5 +1,30 @@
+import pytest
+from unittest.mock import patch, mock_open, MagicMock
 from Assembler.Parser import Parser
 
-def test_instantiation():
-    parser = Parser("an_assembly_file.asm")
-    assert parser._file == "an_assembly_file.asm"
+@pytest.fixture(scope="function")
+def parser():
+    return Parser("an_assembly_file.asm")
+
+def test_instantiation(parser):
+    assert parser.get_file_path() == "an_assembly_file.asm"
+
+def test_open(parser):
+    assert parser._file is None
+    with patch("builtins.open", mock_open(read_data="")) as assembly_file:
+        parser.open_file()
+    assert parser._file.readlines() == []
+    assembly_file.assert_called_once_with("an_assembly_file.asm", "r")
+
+def test_close_on_unopened_file(parser):
+    assert parser._file is None
+    parser.close_file()
+    assert parser._file is None
+
+def test_close_on_open_file(parser):
+    parser._file = MagicMock()
+    mock_close = MagicMock(side_effect=None)
+    parser._file.close = mock_close
+    parser.close_file()
+    mock_close.assert_called_once()
+    assert parser._file is None
