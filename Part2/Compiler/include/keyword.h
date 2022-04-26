54988a5440b0c05af151ed42e02045c118e482c3
MattB17 <matt.buckley1755@gmail.com>
2022-04-25 21:01:32 -0400

    Adding an enum for keyword and the make file

---
+++ Part2/Compiler/include/keyword.h
@@ -0,0 +1,31 @@
+// An Enum representing the collection of keywords in the Jack language.
+#ifndef KEYWORD_H
+#define KEYWORD_H
+
+enum Keyword {
+  // The set of possible keyworkds
+  CLASS = 0,
+  METHOD = 1,
+  FUNCTION = 2,
+  CONSTRUCTOR = 3,
+  INT = 4,
+  BOOLEAN = 5,
+  CHAR = 6,
+  VOID = 7,
+  VAR = 8,
+  STATIC = 9,
+  FIELD = 10,
+  LET = 11,
+  DO = 12,
+  IF = 13,
+  ELSE = 14,
+  WHILE = 15,
+  RETURN = 16,
+  TRUE = 17,
+  FALSE = 18,
+  NULL = 19,
+  THIS = 20,
+  UNKNOWN = 21
+};
+
+#endif KEYWORD_H
