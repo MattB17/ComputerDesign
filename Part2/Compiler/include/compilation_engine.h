10b225f2304dedce00b056788f653d2e07013668
MattB17 <matt.buckley1755@gmail.com>
2022-04-29 18:12:43 -0400

    Setting up ability to tag toekns

--- Part2/Compiler/include/compilation_engine.h
+++ Part2/Compiler/include/compilation_engine.h
@@ -18,8 +18,23 @@ public:
   CompilationEngine(CompilationEngine&&) = delete;
   CompilationEngine &operator=(CompilationEngine&&) = delete;
   ~CompilationEngine() {}
+
+  // Runs the compilation process. Translating the jack file to an XML file.
+  void compile();
 private:
+  // Writes the current token to the XML stream with the appropriate tag.
+  void writeTokenWithTag();
+
+  // Writes the XML opening tag for `tag`.
+  void writeOpenTag(std::string tag);
+
+  // Writes the XML closing tag for `tag`.
+  void writeCloseTag(std::string tag);
+
+  // The tokenizer used to retrieve tokens from the jack file being compiled.
   std::unique_ptr<Tokenizer> tokenizer_;
+
+  // A stream representing the to which the compilation engine writes output.
   std::ofstream xml_stream_;
 };
 
--- Part2/Compiler/src/compilation_engine.cc
+++ Part2/Compiler/src/compilation_engine.cc
@@ -1,9 +1,62 @@
 #include "compilation_engine.h"
 
+#include <string>
+
 #include "util.h"
 
 CompilationEngine::CompilationEngine(std::string jack_file)
   : tokenizer_(std::make_unique<Tokenizer>(jack_file))
 {
   xml_stream_.open(jackFileToXmlFile(jack_file));
+}
+
+void CompilationEngine::compile() {
+  while (tokenizer_->hasMoreTokens()) {
+    tokenizer_->advance();
+    writeTokenWithTag();
+    xml_stream_ << '\n';
+  }
+}
+
+void CompilationEngine::writeTokenWithTag() {
+  std::string tag;
+  switch (tokenizer_->getTokenType()) {
+    case TokenType::KEYWORD:
+      tag = "keyword";
+      writeOpenTag(tag);
+      xml_stream_ << static_cast<int>(tokenizer_->getKeyword());
+      break;
+    case TokenType::SYMBOL:
+      tag = "symbol";
+      writeOpenTag(tag);
+      xml_stream_ << tokenizer_->getSymbol();
+      break;
+    case TokenType::IDENTIFIER:
+      tag = "identifier";
+      writeOpenTag(tag);
+      xml_stream_ << tokenizer_->getIdentifier();
+      break;
+    case TokenType::INT_CONST:
+      tag = "intConstant";
+      writeOpenTag(tag);
+      xml_stream_ << tokenizer_->getIntVal();
+      break;
+    case TokenType::STRING_CONST:
+      tag = "stringConstant";
+      writeOpenTag(tag);
+      xml_stream_ << tokenizer_->getStringVal();
+      break;
+    default:
+      tag = "unknown";
+      writeOpenTag(tag);
+  }
+  writeCloseTag(tag);
+}
+
+void CompilationEngine::writeOpenTag(std::string tag) {
+  xml_stream_ << "<" << tag << "> ";
+}
+
+void CompilationEngine::writeCloseTag(std::string tag) {
+  xml_stream_ << " </" << tag << ">";
 }
